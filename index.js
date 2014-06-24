e48548ddea765f6dbfed68465ac911727cb31425
Péter Gombos <pego@bdo.no>
2014-06-24 12:28:07 +0200

    Make angular module

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+var angular = require('angular')
 
 /**
  * Module dependencies.
  */
-var foo = 'bar';
+var user = angular.module('User');
 
+user.config(function (){	
+})
+
+
+module.exports = user;
