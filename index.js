61530863879823521ea636db40cd373d192ec49e
Henrik Kjelsberg <hkjels@me.com>
2014-06-25 14:42:17 +0200

    Added stringify

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var angular = require('angular');
-require('angular-router-browserify')(angular);
 /**
  * Module dependencies.
  */
+var angular = require('angular');
+require('angular-router-browserify')(angular);
 var user = angular.module('user', ['ngRoute']);
 
 user.config('$routeProvider', function ($routeProvider) {
-	$routeProvider
-		.when('/login', {
-			controller: 'UserController'
-		});
+  $routeProvider
+    .when('/login', {
+      controller: 'UserController'
+    });
 });
 
 
 user.controller('UserController', ['$scope', function ($scope) {
-	$scope.boxes = {
-		name: 'login-module'
-	}
+  $scope.boxes = {
+    name: 'login-module'
+  }
 }]);
 
 user.directive('loginModule', function () {
-	return {
-		template: require('./login.html')
-	}
+  return {
+    template: require('./login.html')
+  }
 });
 
 module.exports = user;
